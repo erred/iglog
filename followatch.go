1481a6455edae58b93de3b152b620c293eb2d328
Sean Liao <seankhliao@gmail.com>
2019-06-19 18:20:03 +0800

    add logging

--- followatch.go
+++ followatch.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/ahmdrz/goinsta"
 	"github.com/seankhliao/iglog/iglog"
+	log "github.com/sirupsen/logrus"
 )
 
 func (c *Client) EventLog(ctx context.Context, r *iglog.Request) (*iglog.Events, error) {
+	log.Infoln("EventLog started")
 	evs := &iglog.Events{}
 	for _, e := range c.followEvents {
 		for _, u := range e.GainedFollowers {
@@ -40,23 +42,28 @@ func (c *Client) EventLog(ctx context.Context, r *iglog.Request) (*iglog.Events,
 			})
 		}
 	}
+	log.Infoln("EventLog done")
 	return evs, nil
 }
 func (c *Client) Followers(ctx context.Context, r *iglog.Request) (*iglog.Users, error) {
+	log.Infoln("Followers started")
 	us := &iglog.Users{}
 	us.Users = make([]*iglog.User, len(c.followDiff.followers))
 	for i, u := range c.followDiff.followers {
 		us.Users[i] = user2proto(u)
 	}
+	log.Infoln("Followers done")
 	return us, nil
 }
 
 func (c *Client) Following(ctx context.Context, r *iglog.Request) (*iglog.Users, error) {
+	log.Infoln("Following started")
 	us := &iglog.Users{}
 	us.Users = make([]*iglog.User, len(c.followDiff.following))
 	for i, u := range c.followDiff.following {
 		us.Users[i] = user2proto(u)
 	}
+	log.Infoln("Following done")
 	return us, nil
 }
 
